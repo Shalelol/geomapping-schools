3592f17eb2e35a592eef772e81c6043b7dd8e6e3
Shale Kuzmanovski <shale.lol@hotmail.com>
2017-09-12 14:56:46 +1000

    Remove header and footer

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -56,10 +56,7 @@ class App extends Component {
   render() {
     return !this.props.error ? (
       <div>
-        <Header />
         {this.props.children}
-        <Feedback />
-        <Footer />
       </div>
     ) : this.props.children;
   }
